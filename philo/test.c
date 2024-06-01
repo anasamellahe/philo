90e78c0a615fb9630a90c5d1a52bbfff4a6a4194
Anas Amellah <anamella@e2r2p7.1337.ma>
2024-06-01 01:30:34 +0100

    test

--- philo/test.c
+++ philo/test.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/wait.h>
 // typedef struct s_philos
 // {
 // 	pthread_mutex_t	left_fork;
@@ -18,39 +18,54 @@
 
 typedef struct s_arg
 {
-	pthread_mutex_t fork1;
-	pthread_mutex_t fork2;
+	pthread_mutex_t *fork_l;
+	pthread_mutex_t *fork_r;
+	pthread_t tid;
+	int id;
 }	t_arg;
 void *print_message(void *arg)
 {
-	t_arg *args;
-
-	args = (t_arg *)arg;
-	pthread_mutex_lock(&args->fork1);
-	printf("hello anas\n");
-	pthread_mutex_unlock(&args->fork1);
-	pthread()
+	t_arg *args = (t_arg *)arg;
+	printf("philo id == %d\n", args->id);
+	while (1)
+	{
+		printf("in while %d\n", args->id);
+		pthread_mutex_lock(args->fork_l);
+		printf("philo %d take a fork_l\n", args->id);
+		pthread_mutex_lock(args->fork_r);
+		printf("philo %d take a fork_r\n", args->id);
+		printf("philo %d eat\n", args->id);
+		pthread_mutex_unlock(args->fork_l);
+		printf("philo %d unlock  a for_lk\n", args->id);
+		pthread_mutex_unlock(args->fork_r);
+		printf("philo %d unlock  a fork_r\n", args->id);
+		usleep(200);
+	}
 }
 int main(int ac, char **av)
 {
-	 pthread_mutex_t fork1;
-	 pthread_mutex_t fork2;
-	 pthread_t t1;
-	 pthread_t t2;
-	 t_arg arg;
-	
-	pthread_mutex_init(&fork1, NULL);
-	pthread_mutex_init(&fork2, NULL);
-	arg.fork1 = fork1;
-	arg.fork2 = fork2;
-	pthread_create(&t1, NULL, print_message, &arg);
-	pthread_create(&t2, NULL, print_message, &arg);
+	 t_arg arg1;
+	 t_arg arg2;
 
-	pthread_join(&t1, NULL);
-	pthread_join(&t2, NULL);
+	pthread_mutex_t fork_l;
+	pthread_mutex_t fork_r;
+	arg1.fork_l = &fork_l;
+	arg1.fork_r = &fork_r;
+	arg2.fork_l = &fork_l;
+	arg2.fork_r = &fork_r;
+	arg1.id = 0;
+	arg2.id = 1;
+	pthread_mutex_init(&fork_l, NULL);
+	pthread_mutex_init(&fork_r, NULL);
+	pthread_create(&arg1.tid, NULL, print_message, &arg1);
+	usleep(800);
+	pthread_create(&arg2.tid, NULL, print_message, &arg2);
 
-	pthread_mutex_destroy(&fork1);
-	pthread_mutex_destroy(&fork2);
+	pthread_join(arg1.tid, NULL);
+	pthread_join(arg2.tid, NULL);
+
+	pthread_mutex_destroy(&fork_l);
+	pthread_mutex_destroy(&fork_r);
 
 
 }
